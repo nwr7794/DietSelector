9f425c31becf83d8a9aea589722f3d9cd30aef2a
nwr7794 <nwr7794@gmail.com>
2021-08-29 15:30:30 -0400

    fixed profile

--- assets/js/model.js
+++ assets/js/model.js
@@ -69,11 +69,11 @@
         }
 
         // Set user output summary variables
-        document.getElementById('outSpeed').value = speed*20
-        document.getElementById('outQuantity').value = quantity*20
-        document.getElementById('outLogging').value = (logging*20 - 20)*1.25 + 1
-        document.getElementById('outTime').value = (logging*20 - 20)*1.25 + 1
-        document.getElementById('outCommercial').value = (logging*20 - 20)*1.25 + 1
+        document.getElementById('outSpeed').value = (speed-1)*20 + 1
+        document.getElementById('outQuantity').value = (quantity-1)*20 + 1
+        document.getElementById('outLogging').value = (logging-1)*20 + 1
+        document.getElementById('outTime').value = (timing-1)*20 + 1
+        document.getElementById('outCommercial').value = (commercial-1)*20 + 1
 
         // Say every diet starts with a perfect score (42 points)
         // Speed - Deduct: Abs(Speed diet - speed assumption)
